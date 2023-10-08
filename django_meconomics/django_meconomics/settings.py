47676b137968b6a09e13391ebad52e929e53cdc7
MarisKX <maris.kurpnieks@gmail.com>
2023-10-08 15:28:06 +0200

    Configurated project to run on HTTPS

--- django_meconomics/django_meconomics/settings.py
+++ django_meconomics/django_meconomics/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Add Ons
+    "django_extensions",
     'rest_framework',
     # 'drf_spectacular',
     'rest_framework.authtoken',
     # 'corsheaders',
-    # 'sslserver',
     # Custom Apps
     'core',
 ]
